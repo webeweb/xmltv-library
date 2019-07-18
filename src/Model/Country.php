192a0e3e29a52f40a02aa0a0de7e8cbbef412d3c
webeweb <support@webeweb.fr>
2019-07-18 09:44:35 +0200

    Add Country

---
+++ src/Model/Country.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+/**
+ * Country.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Country extends AbstractModel {
+
+    /**
+     * Get the content.
+     *
+     * @return string Returns the content.
+     */
+    public function getContent() {
+        return parent::getContent();
+    }
+
+    /**
+     * Set the content.
+     *
+     * @param string $content The content.
+     * @return Aspect Returns this aspect.
+     */
+    public function setContent($content) {
+        return parent::setContent($content);
+    }
+}
---
+++ tests/Model/CountryTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\Country;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Country test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class CountryTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Country();
+
+        $this->assertNull($obj->getContent());
+    }
+
+    /**
+     * Tests the setContent() method.
+     *
+     * @return void
+     */
+    public function testSetContent() {
+
+        $obj = new Country();
+
+        $obj->setContent("content");
+        $this->assertEquals("content", $obj->getContent());
+    }
+}
