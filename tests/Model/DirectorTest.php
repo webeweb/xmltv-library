3dc519d47a5cbb169bc255f022a2f31bba61246d
webeweb <support@webeweb.fr>
2019-07-18 09:41:56 +0200

    Add Director

---
+++ tests/Model/DirectorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\Director;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Director test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class DirectorTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Director();
+
+        $this->assertNull($obj->getContent());
+    }
+}
