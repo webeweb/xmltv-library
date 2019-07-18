910be3a01c2309214cc12178f635b62a256a4c1a
webeweb <support@webeweb.fr>
2019-07-18 09:47:50 +0200

    Add Quality

---
+++ src/Model/Quality.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+/**
+ * Quality.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Quality extends AbstractModel {
+
+    /**
+     * Get the content.
+     *
+     * @return string Returns the content.
+     */
+    public function getContent() {
+        return parent::getContent();
+    }
+
+    /**
+     * Set the content.
+     *
+     * @param string $content The content.
+     * @return Quality Returns this quality.
+     */
+    public function setContent($content) {
+        return parent::setContent($content);
+    }
+}
---
+++ tests/Model/QualityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\Quality;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Quality test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class QualityTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Quality();
+
+        $this->assertNull($obj->getContent());
+    }
+
+    /**
+     * Tests the setContent() method.
+     *
+     * @return void
+     */
+    public function testSetContent() {
+
+        $obj = new Quality();
+
+        $obj->setContent("content");
+        $this->assertEquals("content", $obj->getContent());
+    }
+}
