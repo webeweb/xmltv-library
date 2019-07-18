90a5331d491b84f807d00aeb59e0608029d8d52f
webeweb <support@webeweb.fr>
2019-07-18 09:48:53 +0200

    Add Title

---
+++ src/Model/Title.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+/**
+ * Title.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Title extends AbstractModel {
+
+    /**
+     * Get the content.
+     *
+     * @return string Returns the content.
+     */
+    public function getContent() {
+        return parent::getContent();
+    }
+
+    /**
+     * Set the content.
+     *
+     * @param string $content The content.
+     * @return DisplayName Returns this display name.
+     */
+    public function setContent($content) {
+        return parent::setContent($content);
+    }
+}
