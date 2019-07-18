69cc66cf8ba2dba00e1d42d6d46e90ef168b8779
webeweb <support@webeweb.fr>
2019-07-18 09:49:33 +0200

    Add Value

---
+++ src/Model/Value.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+/**
+ * Value.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Value extends AbstractModel {
+
+    /**
+     * Get the content.
+     *
+     * @return string Returns the content.
+     */
+    public function getContent() {
+        return parent::getContent();
+    }
+
+    /**
+     * Set the content.
+     *
+     * @param string $content The content.
+     * @return Value Returns this value.
+     */
+    public function setContent($content) {
+        return parent::setContent($content);
+    }
+}
