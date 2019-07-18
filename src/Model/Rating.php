00907c7b2b2739812222879763f1dbdb2eaa2494
webeweb <support@webeweb.fr>
2019-07-18 09:50:08 +0200

    Add Rating

---
+++ src/Model/Rating.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+/**
+ * Rating.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Rating extends AbstractModel {
+
+    /**
+     * Icon.
+     *
+     * @var Icon
+     */
+    private $icon;
+
+    /**
+     * System.
+     *
+     * @var string
+     */
+    private $system;
+
+    /**
+     * Value.
+     *
+     * @var Value
+     */
+    private $value;
+
+    /**
+     * Get the icon.
+     *
+     * @return Icon Returns the icon.
+     */
+    public function getIcon() {
+        return $this->icon;
+    }
+
+    /**
+     * Get tje system.
+     *
+     * @return string Returns the system.
+     */
+    public function getSystem() {
+        return $this->system;
+    }
+
+    /**
+     * Get the value.
+     *
+     * @return Value Returns the value.
+     */
+    public function getValue() {
+        return $this->value;
+    }
+
+    /**
+     * Set the icon.
+     *
+     * @param Icon|null $icon The icon.
+     * @return Rating Returns this rating.
+     */
+    public function setIcon(Icon $icon = null ) {
+        $this->icon = $icon;
+        return $this;
+    }
+
+    /**
+     * Set the system.
+     *
+     * @param string $system The system.
+     * @return Rating Returns this rating.
+     */
+    public function setSystem($system) {
+        $this->system = $system;
+        return $this;
+    }
+
+    /**
+     * Set the value.
+     *
+     * @param Value|null $value The value.
+     * @return Rating Returns this rating.
+     */
+    public function setValue(Value $value = null) {
+        $this->value = $value;
+        return $this;
+    }
+}
---
+++ tests/Model/RatingTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\Icon;
+use WBW\Library\XMLTV\Model\Rating;
+use WBW\Library\XMLTV\Model\Value;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Rating test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class RatingTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Rating();
+
+        $this->assertNull($obj->getIcon());
+        $this->assertNull($obj->getSystem());
+        $this->assertNull($obj->getValue());
+    }
+
+    /**
+     * Tests the setIcon() method.
+     *
+     * @return void
+     */
+    public function testSetIcon() {
+
+        // Set an Icon mock.
+        $aspect = new Icon();
+
+        $obj = new Rating();
+
+        $obj->setIcon($aspect);
+        $this->assertSame($aspect, $obj->getIcon());
+    }
+
+    /**
+     * Tests the setSystem() method.
+     *
+     * @return void
+     */
+    public function testSetSystem() {
+
+        $obj = new Rating();
+
+        $obj->setSystem("system");
+        $this->assertEquals("system", $obj->getSystem());
+    }
+
+    /**
+     * Tests the setValue() method.
+     *
+     * @return void
+     */
+    public function testSetValue() {
+
+        // Set an Value mock.
+        $aspect = new Value();
+
+        $obj = new Rating();
+
+        $obj->setValue($aspect);
+        $this->assertSame($aspect, $obj->getValue());
+    }
+}
