6b2a5aa6e1788d63a5f76d02f48edbcb24d6bb0e
webeweb <support@webeweb.fr>
2019-07-23 16:03:52 +0200

    Add Producer

---
+++ tests/Model/ProducerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\Producer;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Producer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class ProducerTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Producer();
+
+        $this->assertNull($obj->getContent());
+    }
+}
