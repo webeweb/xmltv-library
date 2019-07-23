47b6890e13a3e274f1189d21c88f35070de953c5
webeweb <support@webeweb.fr>
2019-07-23 13:08:21 +0200

    Add Language

---
+++ src/Model/Language.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+use WBW\Library\XMLTV\Traits\ContentTrait;
+use WBW\Library\XMLTV\Traits\LangTrait;
+
+/**
+ * Language.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Language extends AbstractModel {
+
+    use ContentTrait;
+    use LangTrait;
+}
