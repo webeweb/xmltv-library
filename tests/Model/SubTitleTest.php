578d65aabfb057f61adafe3c3cd3fd223581b0b5
webeweb <support@webeweb.fr>
2019-07-23 12:13:56 +0200

    Add Sub title

---
+++ tests/Model/SubTitleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\SubTitle;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Sub title test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class SubTitleTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SubTitle();
+
+        $this->assertNull($obj->getContent());
+        $this->assertNull($obj->getLang());
+    }
+}
