2027d2b1412a54d238243d5730f98e1d2bfa8151
webeweb <support@webeweb.fr>
2019-07-23 13:06:35 +0200

    Add Colour

---
+++ src/Model/Colour.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+use WBW\Library\XMLTV\Traits\ContentTrait;
+
+/**
+ * Colour.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Colour extends AbstractModel {
+
+    use ContentTrait;
+}
