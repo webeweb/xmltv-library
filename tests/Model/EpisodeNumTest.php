7dc7dd846d78650aab915c708ab049e3ec959a26
webeweb <support@webeweb.fr>
2019-07-23 14:54:20 +0200

    Add Episode num

---
+++ tests/Model/EpisodeNumTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\EpisodeNum;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Episode num test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class EpisodeNumTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new EpisodeNum();
+
+        $this->assertNull($obj->getContent());
+        $this->assertNull($obj->getSystem());
+    }
+}
