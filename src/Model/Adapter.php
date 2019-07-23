c97984b9cde1c1b677cebc6c5358ec1fdbbe9c43
webeweb <support@webeweb.fr>
2019-07-23 16:03:08 +0200

    Add Adapter

---
+++ src/Model/Adapter.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Model;
+
+/**
+ * Adapter.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Model
+ */
+class Adapter extends AbstractCredit {
+
+}
---
+++ tests/Model/AdapterTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Model;
+
+use WBW\Library\XMLTV\Model\Adapter;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+
+/**
+ * Adapter test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Model
+ */
+class AdapterTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Adapter();
+
+        $this->assertNull($obj->getContent());
+    }
+}
