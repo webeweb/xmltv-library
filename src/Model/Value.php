72c0dc8fef26d80f36a771c313135c6e18caccfd
webeweb <support@webeweb.fr>
2019-07-23 12:10:14 +0200

    Update Value: - use Content trait

--- src/Model/Value.php
+++ src/Model/Value.php
@@ -11,6 +11,8 @@
 
 namespace WBW\Library\XMLTV\Model;
 
+use WBW\Library\XMLTV\Traits\ContentTrait;
+
 /**
  * Value.
  *
@@ -19,22 +21,5 @@ namespace WBW\Library\XMLTV\Model;
  */
 class Value extends AbstractModel {
 
-    /**
-     * Get the content.
-     *
-     * @return string Returns the content.
-     */
-    public function getContent() {
-        return parent::getContent();
-    }
-
-    /**
-     * Set the content.
-     *
-     * @param string $content The content.
-     * @return Value Returns this value.
-     */
-    public function setContent($content) {
-        return parent::setContent($content);
-    }
+    use ContentTrait;
 }
