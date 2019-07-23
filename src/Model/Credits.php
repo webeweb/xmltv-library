8d2f73977bc959f416a30d2758c695d98288284c
webeweb <support@webeweb.fr>
2019-07-23 16:27:00 +0200

    Update Credits: - add adapters attribute - add commentators attribute - add editors attribute - add presenters attributes - add producers attribute

--- src/Model/Credits.php
+++ src/Model/Credits.php
@@ -27,6 +27,20 @@ class Credits extends AbstractModel {
     private $actors;
 
     /**
+     * Adapters.
+     *
+     * @var Adapter[]
+     */
+    private $adapters;
+
+    /**
+     * Commentators.
+     *
+     * @var Commentator[]
+     */
+    private $commentators;
+
+    /**
      * Composers.
      *
      * @var Composer[]
@@ -41,11 +55,32 @@ class Credits extends AbstractModel {
     private $directors;
 
     /**
+     * Editors.
+     *
+     * @var Editor[]
+     */
+    private $editors;
+
+    /**
      * Guests.
      *
      * @var Guest[]
      */
     private $guests;
+
+    /**
+     * Presenters.
+     *
+     * @var Presenter[]
+     */
+    private $presenters;
+
+    /**
+     * Producers.
+     *
+     * @var Producer[]
+     */
+    private $producers;
 
     /**
      * Writers.
@@ -60,9 +95,14 @@ class Credits extends AbstractModel {
     public function __construct() {
         parent::__construct();
         $this->setActors([]);
+        $this->setAdapters([]);
+        $this->setCommentators([]);
         $this->setComposers([]);
-        $this->setGuests([]);
         $this->setDirectors([]);
+        $this->setEditors([]);
+        $this->setGuests([]);
+        $this->setPresenters([]);
+        $this->setProducers([]);
         $this->setWriters([]);
     }
 
@@ -74,6 +114,28 @@ class Credits extends AbstractModel {
      */
     public function addActor(Actor $actor) {
         $this->actors[] = $actor;
+        return $this;
+    }
+
+    /**
+     * Add an adapter.
+     *
+     * @param Adapter $adapter The adapter.
+     * @return Credits Returns this credits.
+     */
+    public function addAdapter(Adapter $adapter) {
+        $this->adapters[] = $adapter;
+        return $this;
+    }
+
+    /**
+     * Add a commentator.
+     *
+     * @param Commentator $commentator The commentator.
+     * @return Credits Returns this credits.
+     */
+    public function addCommentator(Commentator $commentator) {
+        $this->commentators[] = $commentator;
         return $this;
     }
 
@@ -100,6 +162,17 @@ class Credits extends AbstractModel {
     }
 
     /**
+     * Add an editor.
+     *
+     * @param Editor $editor The editor.
+     * @return Credits Returns this credits.
+     */
+    public function addEditor(Editor $editor) {
+        $this->editors[] = $editor;
+        return $this;
+    }
+
+    /**
      * Add a guest.
      *
      * @param Guest $guest The guest.
@@ -107,6 +180,28 @@ class Credits extends AbstractModel {
      */
     public function addGuest(Guest $guest) {
         $this->guests[] = $guest;
+        return $this;
+    }
+
+    /**
+     * Add a presenter.
+     *
+     * @param Presenter $presenter The presenter.
+     * @return Credits Returns this credits.
+     */
+    public function addPresenter(Presenter $presenter) {
+        $this->presenters[] = $presenter;
+        return $this;
+    }
+
+    /**
+     * Add a producer.
+     *
+     * @param Producer $producer The producer.
+     * @return Credits Returns this credits.
+     */
+    public function addProducer(Producer $producer) {
+        $this->producers[] = $producer;
         return $this;
     }
 
@@ -131,6 +226,24 @@ class Credits extends AbstractModel {
     }
 
     /**
+     * Get the adapters.
+     *
+     * @return Adapter[] Returns the adapters.
+     */
+    public function getAdapters() {
+        return $this->adapters;
+    }
+
+    /**
+     * Get the commentators.
+     *
+     * @return Commentator[] Returns the commentators.
+     */
+    public function getCommentators() {
+        return $this->commentators;
+    }
+
+    /**
      * Get the composers.
      *
      * @return Composer[] Returns the composers.
@@ -149,12 +262,39 @@ class Credits extends AbstractModel {
     }
 
     /**
+     * Get the editors.
+     *
+     * @return Editor[] Returns the editors.
+     */
+    public function getEditors() {
+        return $this->editors;
+    }
+
+    /**
      * Get the guests.
      *
      * @return Guest[] Returns the guests.
      */
     public function getGuests() {
         return $this->guests;
+    }
+
+    /**
+     * Get the presenters.
+     *
+     * @return Presenter[] Returns the presenters.
+     */
+    public function getPresenters() {
+        return $this->presenters;
+    }
+
+    /**
+     * Get the producers.
+     *
+     * @return Producer[] Returns the producers.
+     */
+    public function getProducers() {
+        return $this->producers;
     }
 
     /**
@@ -167,6 +307,96 @@ class Credits extends AbstractModel {
     }
 
     /**
+     * Determines if this credits has actors.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasActors() {
+        return 1 <= count($this->actors);
+    }
+
+    /**
+     * Determines if this credits has adapters.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasAdapters() {
+        return 1 <= count($this->adapters);
+    }
+
+    /**
+     * Determines if this credits has commentators.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasCommentators() {
+        return 1 <= count($this->commentators);
+    }
+
+    /**
+     * Determines if this credits has composers.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasComposers() {
+        return 1 <= count($this->composers);
+    }
+
+    /**
+     * Determines if this credits has directors.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasDirectors() {
+        return 1 <= count($this->directors);
+    }
+
+    /**
+     * Determines if this credits has editors.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasEditors() {
+        return 1 <= count($this->editors);
+    }
+
+    /**
+     * Determines if this credits has guests.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasGuests() {
+        return 1 <= count($this->guests);
+    }
+
+    /**
+     * Determines if this credits has presenters.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasPresenters() {
+        return 1 <= count($this->presenters);
+    }
+
+    /**
+     * Determines if this credits has producers.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasProducers() {
+        return 1 <= count($this->producers);
+    }
+
+    /**
+     * Determines if this credits has writers.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasWriters() {
+        return 1 <= count($this->writers);
+    }
+
+    /**
      * Set the actors.
      *
      * @param Actor[] $actors The actors.
@@ -174,6 +404,28 @@ class Credits extends AbstractModel {
      */
     protected function setActors(array $actors) {
         $this->actors = $actors;
+        return $this;
+    }
+
+    /**
+     * Set the adapters.
+     *
+     * @param Adapter[] $adapters The adapters.
+     * @return Credits Returns this credits.
+     */
+    protected function setAdapters(array $adapters) {
+        $this->adapters = $adapters;
+        return $this;
+    }
+
+    /**
+     * Set the commentators.
+     *
+     * @param Commentator[] $commentators The commentators.
+     * @return Credits Returns this credits.
+     */
+    protected function setCommentators(array $commentators) {
+        $this->commentators = $commentators;
         return $this;
     }
 
@@ -200,6 +452,17 @@ class Credits extends AbstractModel {
     }
 
     /**
+     * Set the editors.
+     *
+     * @param Editor[] $editors The editors.
+     * @return Credits Returns this credits.
+     */
+    protected function setEditors(array $editors) {
+        $this->editors = $editors;
+        return $this;
+    }
+
+    /**
      * Set the guests.
      *
      * @param Guest[] $guests The guests.
@@ -207,6 +470,28 @@ class Credits extends AbstractModel {
      */
     protected function setGuests(array $guests) {
         $this->guests = $guests;
+        return $this;
+    }
+
+    /**
+     * Set the presenters.
+     *
+     * @param Presenter[] $presenters The presenters.
+     * @return Credits Returns this credits.
+     */
+    protected function setPresenters(array $presenters) {
+        $this->presenters = $presenters;
+        return $this;
+    }
+
+    /**
+     * Set the producers.
+     *
+     * @param Producer[] $producers The producers.
+     * @return Credits Returns this credits.
+     */
+    protected function setProducers(array $producers) {
+        $this->producers = $producers;
         return $this;
     }
 
@@ -220,5 +505,4 @@ class Credits extends AbstractModel {
         $this->writers = $writers;
         return $this;
     }
-
 }
--- tests/Model/CreditsTest.php
+++ tests/Model/CreditsTest.php
@@ -12,10 +12,15 @@
 namespace WBW\Library\XMLTV\Tests\Model;
 
 use WBW\Library\XMLTV\Model\Actor;
+use WBW\Library\XMLTV\Model\Adapter;
+use WBW\Library\XMLTV\Model\Commentator;
 use WBW\Library\XMLTV\Model\Composer;
 use WBW\Library\XMLTV\Model\Credits;
 use WBW\Library\XMLTV\Model\Director;
+use WBW\Library\XMLTV\Model\Editor;
 use WBW\Library\XMLTV\Model\Guest;
+use WBW\Library\XMLTV\Model\Presenter;
+use WBW\Library\XMLTV\Model\Producer;
 use WBW\Library\XMLTV\Model\Writer;
 use WBW\Library\XMLTV\Tests\AbstractTestCase;
 
@@ -42,6 +47,43 @@ class CreditsTest extends AbstractTestCase {
         $obj->addActor($actor);
         $this->assertCount(1, $obj->getActors());
         $this->assertSame($actor, $obj->getActors()[0]);
+        $this->assertTrue($obj->hasActors());
+    }
+
+    /**
+     * Tests the addAdapter() method.
+     *
+     * @return void
+     */
+    public function testAddAdapter() {
+
+        // Set an Adapter mock.
+        $adapter = new Adapter();
+
+        $obj = new Credits();
+
+        $obj->addAdapter($adapter);
+        $this->assertCount(1, $obj->getAdapters());
+        $this->assertSame($adapter, $obj->getAdapters()[0]);
+        $this->assertTrue($obj->hasAdapters());
+    }
+
+    /**
+     * Tests the addCommentator() method.
+     *
+     * @return void
+     */
+    public function testAddCommentator() {
+
+        // Set an Commentator mock.
+        $commentator = new Commentator();
+
+        $obj = new Credits();
+
+        $obj->addCommentator($commentator);
+        $this->assertCount(1, $obj->getCommentators());
+        $this->assertSame($commentator, $obj->getCommentators()[0]);
+        $this->assertTrue($obj->hasCommentators());
     }
 
     /**
@@ -59,6 +101,7 @@ class CreditsTest extends AbstractTestCase {
         $obj->addComposer($composer);
         $this->assertCount(1, $obj->getComposers());
         $this->assertSame($composer, $obj->getComposers()[0]);
+        $this->assertTrue($obj->hasComposers());
     }
 
     /**
@@ -76,6 +119,25 @@ class CreditsTest extends AbstractTestCase {
         $obj->addDirector($director);
         $this->assertCount(1, $obj->getDirectors());
         $this->assertSame($director, $obj->getDirectors()[0]);
+        $this->assertTrue($obj->hasDirectors());
+    }
+
+    /**
+     * Tests the addEditor() method.
+     *
+     * @return void
+     */
+    public function testAddEditor() {
+
+        // Set an Editor mock.
+        $editor = new Editor();
+
+        $obj = new Credits();
+
+        $obj->addEditor($editor);
+        $this->assertCount(1, $obj->getEditors());
+        $this->assertSame($editor, $obj->getEditors()[0]);
+        $this->assertTrue($obj->hasEditors());
     }
 
     /**
@@ -93,6 +155,43 @@ class CreditsTest extends AbstractTestCase {
         $obj->addGuest($guest);
         $this->assertCount(1, $obj->getGuests());
         $this->assertSame($guest, $obj->getGuests()[0]);
+        $this->assertTrue($obj->hasGuests());
+    }
+
+    /**
+     * Tests the addPresenter() method.
+     *
+     * @return void
+     */
+    public function testAddPresenter() {
+
+        // Set an Presenter mock.
+        $presenter = new Presenter();
+
+        $obj = new Credits();
+
+        $obj->addPresenter($presenter);
+        $this->assertCount(1, $obj->getPresenters());
+        $this->assertSame($presenter, $obj->getPresenters()[0]);
+        $this->assertTrue($obj->hasPresenters());
+    }
+
+    /**
+     * Tests the addProducer() method.
+     *
+     * @return void
+     */
+    public function testAddProducer() {
+
+        // Set an Producer mock.
+        $producer = new Producer();
+
+        $obj = new Credits();
+
+        $obj->addProducer($producer);
+        $this->assertCount(1, $obj->getProducers());
+        $this->assertSame($producer, $obj->getProducers()[0]);
+        $this->assertTrue($obj->hasProducers());
     }
 
     /**
@@ -110,6 +209,7 @@ class CreditsTest extends AbstractTestCase {
         $obj->addWriter($writer);
         $this->assertCount(1, $obj->getWriters());
         $this->assertSame($writer, $obj->getWriters()[0]);
+        $this->assertTrue($obj->hasWriters());
     }
 
     /**
@@ -122,9 +222,24 @@ class CreditsTest extends AbstractTestCase {
         $obj = new Credits();
 
         $this->assertEquals([], $obj->getActors());
+        $this->assertEquals([], $obj->getAdapters());
+        $this->assertEquals([], $obj->getCommentators());
         $this->assertEquals([], $obj->getComposers());
         $this->assertEquals([], $obj->getDirectors());
+        $this->assertEquals([], $obj->getEditors());
         $this->assertEquals([], $obj->getGuests());
+        $this->assertEquals([], $obj->getPresenters());
+        $this->assertEquals([], $obj->getProducers());
         $this->assertEquals([], $obj->getWriters());
+        $this->assertFalse($obj->hasActors());
+        $this->assertFalse($obj->hasAdapters());
+        $this->assertFalse($obj->hasCommentators());
+        $this->assertFalse($obj->hasComposers());
+        $this->assertFalse($obj->hasDirectors());
+        $this->assertFalse($obj->hasEditors());
+        $this->assertFalse($obj->hasGuests());
+        $this->assertFalse($obj->hasPresenters());
+        $this->assertFalse($obj->hasProducers());
+        $this->assertFalse($obj->hasWriters());
     }
 }
