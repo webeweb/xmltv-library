e0ebe769dba776e445cfcbe927123422dbf6c616
webeweb <support@webeweb.fr>
2019-07-24 15:00:24 +0200

    Add Language trait

---
+++ tests/Traits/LanguageTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Traits;
+
+use WBW\Library\XMLTV\Model\Language;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+use WBW\Library\XMLTV\Tests\Fixtures\Traits\TestLanguageTrait;
+
+/**
+ * Language trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Traits
+ */
+class LanguageTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestLanguageTrait();
+
+        $this->assertNull($obj->getLanguage());
+    }
+
+    /**
+     * Tests the setLanguage() method.
+     *
+     * @return void
+     */
+    public function testSetLanguage() {
+
+        // Set a Language mock.
+        $language = new Language();
+
+        $obj = new TestLanguageTrait();
+
+        $obj->setLanguage($language);
+        $this->assertSame($language, $obj->getLanguage());
+    }
+}
