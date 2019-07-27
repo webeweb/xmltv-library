f5f1ff2571d1a7b2fc2adbe747bda2c3bb5de554
webeweb <support@webeweb.fr>
2019-07-27 13:54:52 +0200

    Add Editors trait

---
+++ src/Traits/EditorsTrait.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Traits;
+
+use WBW\Library\XMLTV\Model\Editor;
+
+/**
+ * Editors trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Traits
+ */
+trait EditorsTrait {
+
+    /**
+     * Editors.
+     *
+     * @var Editor[]
+     */
+    private $editors;
+
+    /**
+     * Add an editor.
+     *
+     * @param Editor $editor The editor.
+     */
+    public function addEditor(Editor $editor) {
+        $this->editors[] = $editor;
+        return $this;
+    }
+
+    /**
+     * Get the editors.
+     *
+     * @return Editor[] Returns the editors.
+     */
+    public function getEditors() {
+        return $this->editors;
+    }
+
+    /**
+     * Determines if this instance has editors.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasEditors() {
+        return 1 <= count($this->editors);
+    }
+
+    /**
+     * Set the editors.
+     *
+     * @param Editor[] $editors The editors.
+     */
+    protected function setEditors(array $editors) {
+        $this->editors = $editors;
+        return $this;
+    }
+}
