06fd8c6e47a51e17a8087440dd02980216c35f33
webeweb <support@webeweb.fr>
2019-07-27 15:01:51 +0200

    Add Sub-titles trait

---
+++ tests/Fixtures/Traits/TestSubTitlesTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Fixtures\Traits;
+
+use WBW\Library\XMLTV\Traits\SubTitlesTrait;
+
+/**
+ * Test sub-titles trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Fixtures\Traits
+ */
+class TestSubTitlesTrait {
+
+    use SubTitlesTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->setSubTitles([]);
+    }
+}
