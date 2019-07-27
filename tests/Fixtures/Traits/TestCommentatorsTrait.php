d5c012155fb61d5f153fb2f95e6f99086aaf8272
webeweb <support@webeweb.fr>
2019-07-27 13:53:51 +0200

    Add Commentators trait

---
+++ tests/Fixtures/Traits/TestCommentatorsTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Fixtures\Traits;
+
+use WBW\Library\XMLTV\Traits\CommentatorsTrait;
+
+/**
+ * Test commentators trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Fixtures\Traits
+ */
+class TestCommentatorsTrait {
+
+    use CommentatorsTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->setCommentators([]);
+    }
+}
