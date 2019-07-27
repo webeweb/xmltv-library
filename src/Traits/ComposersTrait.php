97de883c3d926911cc47b174e98034e129259849
webeweb <support@webeweb.fr>
2019-07-27 13:54:10 +0200

    Add Composers trait

---
+++ src/Traits/ComposersTrait.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Traits;
+
+use WBW\Library\XMLTV\Model\Composer;
+
+/**
+ * Composers trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Traits
+ */
+trait ComposersTrait {
+
+    /**
+     * Composers.
+     *
+     * @var Composer[]
+     */
+    private $composers;
+
+    /**
+     * Add an composer.
+     *
+     * @param Composer $composer The composer.
+     */
+    public function addComposer(Composer $composer) {
+        $this->composers[] = $composer;
+        return $this;
+    }
+
+    /**
+     * Get the composers.
+     *
+     * @return Composer[] Returns the composers.
+     */
+    public function getComposers() {
+        return $this->composers;
+    }
+
+    /**
+     * Determines if this instance has composers.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasComposers() {
+        return 1 <= count($this->composers);
+    }
+
+    /**
+     * Set the composers.
+     *
+     * @param Composer[] $composers The composers.
+     */
+    protected function setComposers(array $composers) {
+        $this->composers = $composers;
+        return $this;
+    }
+}
---
+++ tests/Traits/ComposersTraitTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Tests\Traits;
+
+use WBW\Library\XMLTV\Model\Composer;
+use WBW\Library\XMLTV\Tests\AbstractTestCase;
+use WBW\Library\XMLTV\Tests\Fixtures\Traits\TestComposersTrait;
+
+/**
+ * Composers trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Tests\Traits
+ */
+class ComposersTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the addComposer() method.
+     *
+     * @return void
+     */
+    public function testAddComposer() {
+
+        // Set an Composer mock.
+        $composer = new Composer();
+
+        $obj = new TestComposersTrait();
+
+        $obj->addComposer($composer);
+        $this->assertCount(1, $obj->getComposers());
+        $this->assertSame($composer, $obj->getComposers()[0]);
+        $this->assertTrue($obj->hasComposers());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestComposersTrait();
+
+        $this->assertEquals([], $obj->getComposers());
+        $this->assertFalse($obj->hasComposers());
+    }
+}
