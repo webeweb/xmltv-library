2d51d53ed235123d2a1fb0e474faf89e79f71e6f
webeweb <support@webeweb.fr>
2019-07-27 15:00:25 +0200

    Add Descs trait

---
+++ src/Traits/DescsTrait.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the xmltv-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\XMLTV\Traits;
+
+use WBW\Library\XMLTV\Model\Desc;
+
+/**
+ * Descriptions trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\XMLTV\Traits
+ */
+trait DescsTrait {
+
+    /**
+     * Descriptions.
+     *
+     * @var Desc[]
+     */
+    private $descs;
+
+    /**
+     * Add an description.
+     *
+     * @param Desc $desc The description.
+     * @return DescsTrait Returns this descriptions trait.
+     */
+    public function addDesc(Desc $desc) {
+        $this->descs[] = $desc;
+        return $this;
+    }
+
+    /**
+     * Get the descriptions.
+     *
+     * @return Desc[] Returns the descriptions.
+     */
+    public function getDescs() {
+        return $this->descs;
+    }
+
+    /**
+     * Determines if this instance has descriptions.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasDescs() {
+        return 1 <= count($this->descs);
+    }
+
+    /**
+     * Set the descriptions.
+     *
+     * @param Desc[] $descs The descriptions.
+     * @return DescsTrait Returns this descriptions trait.
+     */
+    protected function setDescs(array $descs) {
+        $this->descs = $descs;
+        return $this;
+    }
+}
