630b01876afe5c805ecf4d05fb3fbb63c224e92b
webeweb <support@webeweb.fr>
2024-03-11 12:57:25 +0100

    Update Stereo model: - fix PHPStan warnings

--- src/Model/Stereo.php
+++ src/Model/Stereo.php
@@ -85,6 +85,7 @@ class Stereo extends AbstractModel {
 
     /**
      * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
      */
     public function jsonSerialize(): array {
         return JsonSerializer::serializeStereo($this);
