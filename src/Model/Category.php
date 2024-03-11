abf11627acce2c196821337b66f6e0f525ae231c
webeweb <support@webeweb.fr>
2024-03-11 12:54:26 +0100

    Update Category model: - fix PHPStan warnings

--- src/Model/Category.php
+++ src/Model/Category.php
@@ -36,6 +36,7 @@ class Category extends AbstractModel {
 
     /**
      * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
      */
     public function jsonSerialize(): array {
         return JsonSerializer::serializeCategory($this);
