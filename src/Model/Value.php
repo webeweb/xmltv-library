123bcee18eaed396f11f1d3693283aabc128c390
webeweb <support@webeweb.fr>
2024-03-11 12:57:54 +0100

    Update Value model: - fix PHPStan warnings

--- src/Model/Value.php
+++ src/Model/Value.php
@@ -34,6 +34,7 @@ class Value extends AbstractModel {
 
     /**
      * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
      */
     public function jsonSerialize(): array {
         return JsonSerializer::serializeValue($this);
