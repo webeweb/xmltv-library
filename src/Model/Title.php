bd579b0842827a18f2989b06973544a1c6757ae7
webeweb <support@webeweb.fr>
2024-03-11 12:57:38 +0100

    Update Title model: - fix PHPStan warnings

--- src/Model/Title.php
+++ src/Model/Title.php
@@ -36,6 +36,7 @@ class Title extends AbstractModel {
 
     /**
      * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
      */
     public function jsonSerialize(): array {
         return JsonSerializer::serializeTitle($this);
