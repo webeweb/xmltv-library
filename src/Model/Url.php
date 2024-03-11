cdc03902d5b030247d0a3c9b8e2b123a86fa4545
webeweb <support@webeweb.fr>
2024-03-11 12:57:49 +0100

    Update URL model: - fix PHPStan warnings

--- src/Model/Url.php
+++ src/Model/Url.php
@@ -34,6 +34,7 @@ class Url extends AbstractModel {
 
     /**
      * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
      */
     public function jsonSerialize(): array {
         return JsonSerializer::serializeUrl($this);
